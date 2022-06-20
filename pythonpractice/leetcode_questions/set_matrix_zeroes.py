20abd21dfb4d75a5621dcc09eb3859e21354da5e
Jiawei <jiaweilim97@gmail.com>
2022-06-21 02:13:37 +0800

    save progress 20/6/2022

---
+++ pythonpractice/leetcode_questions/set_matrix_zeroes.py
@@ -0,0 +1,51 @@
+# matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+
+class Solution():
+    def __init__(self, matrix) -> None:
+        self.matrix = matrix
+        self.num_of_rows = len(matrix)
+        self.num_of_cols = len(matrix[0])
+
+    def print_matrix(self):
+        for row in self.matrix:
+            print(row)
+
+    def set_zeroes(self):
+        col_to_zeroes = []
+        row_to_zeroes = []
+        for r in range(self.num_of_rows):
+            for c in range(self.num_of_cols):
+                if self.matrix[r][c] == 0:
+                    row_to_zeroes.append(r)
+                    col_to_zeroes.append(c)
+        for row in range(self.num_of_rows):
+            if row in row_to_zeroes:
+                self.matrix[row] = [0]*self.num_of_cols
+            else:
+                for col in range(self.num_of_cols):
+                    if col in col_to_zeroes:
+                        self.matrix[row][col] = 0
+            
+    # this solution will set the whole row to zero and then cause the remaining values to screw up as they will be changed since we are doing in place
+    def set_zeroes2(self):
+        col_to_zeroes = []
+        row_to_zeroes = []
+        for r in range(self.num_of_rows):
+            for c in range(self.num_of_cols):
+                if c in col_to_zeroes:
+                    self.matrix[r][c] == 0
+                    print(self.matrix[r][c])
+                    continue # move right to the next col in this row
+                if self.matrix[r][c] == 0:
+                    if r not in row_to_zeroes:
+                        self.matrix[r] = [0]*self.num_of_cols
+                    row_to_zeroes.append(r)
+                    col_to_zeroes.append(c)
+                    break # exit checking the remaining cols in this row (this break only exits the inner for loop)
+
+
+solution = Solution(matrix)
+solution.print_matrix()
+solution.set_zeroes()
+solution.print_matrix()
---
+++ pythonpractice/leetcode_questions/spiral_matrix.py
@@ -0,0 +1,74 @@
+import enum
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# matrix = [[1],[2],[3],[4]]
+# matrix = [[1,2,3,4]]
+# matrix = [[1]]
+
+class Direction(enum.Enum):
+    right = (1, 0) # (x, y) direction
+    down = (0, 1)
+    left = (-1, 0)
+    up = (0, -1)
+
+class Solution():
+    def __init__(self, matrix) -> None:
+        self.matrix = matrix
+        self.matrix_size = len(matrix)*(len(matrix[0]))
+        self.ans = []
+        self.direction = Direction.right
+        self.first_col_index = 0
+        self.last_col_index = len(matrix[0])-1
+        self.first_row_index = 0
+        self.last_row_index = len(matrix)-1
+    
+    def print_ans(self):
+        print(self.ans)
+
+    # recursive spiral
+    def spiral(self, x, y):
+        print(x, y, self.matrix[y][x])
+        self.ans.append(self.matrix[y][x]) # y in row and x is col, dont be careless
+        if len(self.ans) == self.matrix_size: # spiral ended
+            return
+        if self.direction == Direction.right:
+            print("right")
+            if x == self.last_col_index:
+                print("going down")
+                self.direction = Direction.down
+                self.first_row_index += 1
+                self.spiral(x + Direction.down.value[0], y + Direction.down.value[1])
+            else: # continue right
+                self.spiral(x + Direction.right.value[0], y + Direction.right.value[1])
+        elif self.direction == Direction.down:
+            print("down")
+            if y == self.last_row_index:
+                print("going left")
+                self.direction = Direction.left
+                self.last_col_index -= 1
+                self.spiral(x + Direction.left.value[0], y + Direction.left.value[1])
+            else: # continue down
+                self.spiral(x + Direction.down.value[0], y + Direction.down.value[1])
+        elif self.direction == Direction.left:
+            print("left")
+            if x == self.first_col_index:
+                print("going up")
+                self.direction = Direction.up
+                self.last_row_index -= 1
+                self.spiral(x + Direction.up.value[0], y + Direction.up.value[1])
+            else: # continue left
+                self.spiral(x + Direction.left.value[0], y + Direction.left.value[1])
+        else: # up
+            print("up")
+            if y == self.first_row_index:
+                print("going right")
+                self.direction = Direction.right
+                self.first_col_index += 1
+                self.spiral(x + Direction.right.value[0], y + Direction.right.value[1])
+            else: # continue up
+                self.spiral(x + Direction.up.value[0], y + Direction.up.value[1])
+
+
+solution = Solution(matrix)
+solution.spiral(0, 0)
+solution.print_ans()
