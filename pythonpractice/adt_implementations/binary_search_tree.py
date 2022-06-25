0e48cf304593573325d273507eb0116351d20cba
Jiawei <jiaweilim97@gmail.com>
2022-06-25 21:37:32 +0800

    save progress 25/6/2022

--- pythonpractice/adt_implementations/binary_search_tree.py
+++ pythonpractice/adt_implementations/binary_search_tree.py
@@ -29,9 +29,87 @@ class Child(enum.Enum):
 class BST():
     def __init__(self, root=None) -> None:
         self.root = root
+        self.max_height = 0
 
     def get_root(self):
         return self.root
+    
+    def get_max_height(self):
+        return self.max_height
+
+    def find_max_height(self, root, height):
+        if not root:
+            return height
+        height = max(self.find_max_height(root.left, height+1), self.find_max_height(root.right, height+1))
+        return height
+
+    # from smallest to largest left to right to largest to smallest left to right and vice versa
+    def invert_tree(self, root):
+        if not root:
+            return root
+        if not root.right and not root.left:
+            return root
+        temp_left = self.invert_tree(root.right)
+        temp_right = self.invert_tree(root.left)
+        root.left = temp_left
+        root.right = temp_right
+        return root
+
+    # if 2 nodes are siblings, return the first common parent
+    # by definition of LCA, if one node is a parent of the other node, the LCA is the parent node
+    def find_LCA2(self, root, val1, val2):
+        if not root:
+            return None
+        if root.val == val1 or root.val == val2:
+            return root
+        if val1 < root.val:
+            node1 = self.find_LCA(root.left, val1, val2)
+        elif val1 > root.val:
+            node1 = self.find_LCA(root.right, val1, val2)
+        if val2 < root.val:
+            node2 = self.find_LCA(root.left, val1, val2)
+        elif val2 > root.val:
+            node2 = self.find_LCA(root.right, val1, val2)
+        if node1 and node2:
+            return root
+        elif node1 and not node2:
+            return node1
+        elif node2 and not node1:
+            return node2
+        else:
+            return None
+
+    def find_LCA(self, root, val1, val2):
+        if not root:
+            return
+        if root.val == val1 or root.val == val2:
+            return root
+        # if both the val are on the same subtree, break the problem down further
+        if val1 < root.val and val2 < root.val:
+            return self.find_LCA(root.left, val1, val2)
+        elif val1 > root.val and val2 > root.val:
+            return self.find_LCA(root.right, val1, val2)
+        # if they are splitting at any point, then this will be the LCA
+        # the general idea of finding LCA is actually very simple, we just find the point where they start to split
+        # however, my implementation also accounts for whether the 2 nodes can even be found, although the leetcode question doesnt require
+        elif val1 < root.val and val2 > root.val or val1 > root.val and val2 < root.val:
+            if val1 < root.val:
+                node1 = self.find_LCA(root.left, val1, val2)
+                node2 = self.find_LCA(root.right, val1, val2)
+            elif val1 > root.val:
+                node1 = self.find_LCA(root.right, val1, val2)
+                node2 = self.find_LCA(root.left, val1, val2)
+            if node1 and node2: # both vals can be found
+                return root
+            # the following conditions is needed to further check splits which will confirm be taken but will reach None for the other value
+            elif not node1:
+                return node2
+            elif not node2:
+                return node1
+            else:
+                return
+        
+            
 
     def insert_node(self, node):
         if not self.root:
@@ -78,7 +156,7 @@ class BST():
     # the magic is that we no need to parents, think of it as connecting 2 arrows into 1
     # this helps in both the finding part and the assigning part
     def delete_node(self, root, val):
-        print(root.val)
+        # we return the root/node as it is part of our recursion
         if not root:
             return root
         if val < root.val:
@@ -88,14 +166,25 @@ class BST():
         else: # val == root.val
             if not root.right and not root.left: # 0 child
                 root = None
-            elif not root.left: # 1 child on the left
+            elif not root.left: # 1 child on the right
                 root = root.right
-            elif not root.right:
+            elif not root.right: # 1 child on the left
                 root = root.left
             elif root.right and root.left: # 2 children
                 successor = self.find_successor(root.right)
                 root.val = successor.val
                 root.right = self.delete_node(root.right, successor.val)
+                # we can either set the predecessor or the successor to replace the node to be deleted
+                # predecessor = self.find_predecessor(root.left)
+                # root.val = predecessor.val
+                # root.left = self.delete_node(root.left, predecessor.val)
+        # the following return root is one way to do recursion
+        # think of it as we enter the left or right child,
+        # but if nothing happens, we have to pass it back to the parent to set it back
+        # hence we have to put the root.right = delete_node() assignment
+        # if something happens (the root is to be deleted), the root value will be changed here and returned back to the parent
+        # for example, when the current node has 0 child and it is the node to be deleted, we set this node(root) to None
+        # then when it is returned to the parent, it will be set as None
         return root
 
 
@@ -170,3 +259,10 @@ bst.print_inorder()
 root = bst.get_root()
 bst.delete_node(root, 3)
 bst.print_inorder()
+root = bst.get_root()
+print(bst.find_max_height(root, 0))
+bst.invert_tree(root)
+bst.print_inorder()
+bst.invert_tree(root)
+bst.print_inorder()
+print(bst.find_LCA(root, 11, 10).val)
