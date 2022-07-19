1bd95211b5d476e0ca2ea2ba6741a8b73905d792
Jiawei <jiaweilim97@gmail.com>
2022-07-19 17:55:55 +0800

    rearranged some file directories and updated merge_k_sorted_lists solution

--- pythonpractice/leetcode_questions/merge_k_sorted_lists.py
+++ pythonpractice/leetcode_questions/merge_k_sorted_lists.py
@@ -1,63 +1,44 @@
-import heapq
+from heapq import heappush, heappop
 import sys
 sys.path.append("..")
-# from adt_implementations import LinkedList, Node
-from reverse_linked_list import LinkedList, Node
+from adt_implementations.linked_list import LinkedList, Node
 
-def print_from_head(head):
-    if not head:
-        print("empty linkedlist")
-        return
-    curr = head
-    while curr:
-        if curr.next:
-            print(curr.val, end=" --> ")
-        else:
-            print(curr.val)
-        curr = curr.next
-
-lists = [[1,4,5],[1,3,4],[2,6]]
-# lists = [[1,2,3]]
-lists_of_linked_lists = []
+# init some input
+lists = [[1,4,5],[1,3,4],[2,6]] # output: [1,1,2,3,4,4,5,6]
+list_of_linked_lists = []
 for list in lists:
-    new_linked_list = LinkedList()
+    linked_list = LinkedList()
     for item in list:
-        new_linked_list.add_node(Node(item))
-    lists_of_linked_lists.append(new_linked_list.get_head())
-
-for linked_list in lists_of_linked_lists:
-    print_from_head(linked_list)
-
+        linked_list.add_node(Node(item))
+    list_of_linked_lists.append(linked_list)
+    linked_list.print_linked_list()
 
 class Solution():
-    def __init__(self, k_lists) -> None:
+    def __init__(self) -> None:
         self.heap = []
-        self.k_lists = k_lists
 
-    def merge(self):
-        head = Node(None)
-        ptr = head
-        # some edge cases for slightly faster performance
-        if len(self.k_lists) == 0:
+    def merge(self, list_of_linked_lists): # returns the first node in the ans linked list
+        ptr = dummy_head = Node(None)
+        # some edge cases to exit early
+        if len(list_of_linked_lists) == 0:
             return None
-        elif len(self.k_lists) == 1:
-            return self.k_lists[0]
-
-        # add all to heap first
-        for list in self.k_lists:
-            curr = list
+        elif len(list_of_linked_lists) == 1:
+            return list_of_linked_lists[0]
+        
+        for list in list_of_linked_lists:
+            curr = list.get_head()
             while curr:
-                heapq.heappush(self.heap, curr.val)
+                heappush(self.heap, curr.val)
                 curr = curr.next
-        # when pop out from heap, will be sorted. by default is min heap
-        while len(self.heap) > 0:
-            ptr.next = Node(heapq.heappop(self.heap))
+        while self.heap:
+            ptr.next = Node(heappop(self.heap))
             ptr = ptr.next
-        return head.next
+        return dummy_head.next
+            
 
-solution = Solution(lists_of_linked_lists)
-print_from_head(solution.merge())
-
-
-
-
+if __name__ == "__main__":
+    solution = Solution()
+    ans = LinkedList()
+    head = solution.merge(list_of_linked_lists)
+    ans.add_node(head)
+    ans.print_linked_list()
